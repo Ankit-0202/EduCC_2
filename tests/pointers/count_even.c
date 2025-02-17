b2ff2b4646df1be23cb7d8aab2f72a308d457631
Ankit Kapoor <ankit.kapoor@sydney.edu.au>
2025-02-18 01:47:56 +1100

    Lint test cases

--- tests/pointers/count_even.c
+++ tests/pointers/count_even.c
@@ -1,11 +1,11 @@
 int main() {
-    int arr[] = {1, 2, 3, 4, 5, 6};
-    int count = 0;
-    int *ptr = arr;
-    int i;
-    for (i = 0; i < 6; i++) {
-        if (*(ptr + i) % 2 == 0)
-            count++;
-    }
-    return count;  // returns 3
+  int arr[] = {1, 2, 3, 4, 5, 6};
+  int count = 0;
+  int *ptr = arr;
+  int i;
+  for (i = 0; i < 6; i++) {
+    if (*(ptr + i) % 2 == 0)
+      count++;
+  }
+  return count; // returns 3
 }
